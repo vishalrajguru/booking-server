2bb3cad0867cfe856ba9f3269b9236f4cee97d81
Vishal Rajguru <69671366+vishalrajguru@users.noreply.github.com>
2023-07-23 17:01:57 +0530

    create room api created (#1)

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,20 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
-router.get("/isauthenticated", verifyToken, (req, res, next)=>{
-        res.send("hello, you are logged in")
-})
-router.put("/:id", updateUser)
-router.delete("/:id", deleteUser)
-router.get("/:id", getUser)
-router.get("/", getUsers)       
+// router.get("/isauthenticated", verifyToken, (req, res, next)=>{
+//         res.send("hello, you are logged in")    
+// })
+// router.get("/checkuser/:id", verifyUser, (req, res, next)=>{
+//         res.send("hello user , you are logged in you can delete your account")    
+// })
+// router.get("/isAdmin/:id", verifyAdmin, (req, res, next)=>{
+//         res.send("hello admin , you are logged in you can delete all account")    
+// })
+router.put("/:id",verifyUser, updateUser)
+router.delete("/:id",verifyUser, deleteUser)
+router.get("/:id", verifyUser, getUser)
+router.get("/",verifyAdmin, getUsers)       
 
-export default router;
+export default router;  
